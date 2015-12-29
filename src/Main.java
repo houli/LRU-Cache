667b3811c2d41e89bbf2f5de66b1243cc451a0d9
Eoin Houlihan <ehoulih@tcd.ie>
2015-12-29 18:12:42 +0000

    Reading text file from JAR actually works

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) {
         Cache cache = new LRUCache(16, 8, 1);
-        try (Stream<String> lines = Files.lines(Paths.get("resources", "addresses.txt"))) {
-            lines.map(line -> Integer.parseInt(line, 16))
+        try (InputStream stream = Main.class.getResourceAsStream("addresses.txt")) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+            reader.lines().map(line -> Integer.parseInt(line, 16))
                 .forEach(address -> {
                     System.out.println(cache.access(address));
                 });
